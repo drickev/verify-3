531d250c47468015ebd62b9e6c0417d6bf7a6404
drickev <apriantohaerul@gmail.com>
2024-06-14 00:18:57 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,14 +4,30 @@ export default {
 	ALLIANCE_LIST: [
 		// add your alliance name + role_id here
 		{
-			name: 'Storm',
-			role_id: '1248297570376220755',
+			name: 'Crimson',
+			role_id: '1127958023172804708',
 		},
 		{
-			name: 'Crimson',
-			role_id: '1250755293147561994',
+			name: 'Storm',
+			role_id: '1127957920043253801',
+		},
+		{
+			name: 'Legends',
+			role_id: '1197824089251905568',
+		},
+		{
+			name: 'Desire',
+			role_id: '1202134843786534923',
+		},
+		{
+			name: 'Phoenix',
+			role_id: '1198274099668066436',
+		},
+		{
+			name: 'Atreides',
+			role_id: '1206597005129023528',
 		}
 
 	],
-	VERIFY_CHANNEL_ID: '1246139767611986062',
+	VERIFY_CHANNEL_ID: '1215996857885655150',
 };
