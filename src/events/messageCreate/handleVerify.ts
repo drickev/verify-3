7991da6d81552a781db277861ca314d291397985
drickev <apriantohaerul@gmail.com>
2024-06-14 00:13:20 +0700

    Update handleVerify.ts

--- src/events/messageCreate/handleVerify.ts
+++ src/events/messageCreate/handleVerify.ts
@@ -1,143 +1,130 @@
-import { AttachmentBuilder, EmbedBuilder, type Message } from 'discord.js';
+import { EmbedBuilder, type Message } from 'discord.js';
 import { createWorker } from 'tesseract.js';
-import sharp from 'sharp';
 import axios from 'axios';
 import config from '../../config';
 import sizeOf from 'image-size';
 
 const topLeftM = { x: 0.39821, y: 0.365672 };
 const buttomRightM = { x: 0.615213, y: 0.402985 };
-const topLeftP = { x: 0.15, y: 0.475556 };
-const buttomRightP = { x: 0.2625, y: 0.5 };
 
 export default async function (message: Message<true>) {
-	if (message.author.bot) return;
 	if (
-		message.attachments.size > 0 &&
-		message.channel.id === config.VERIFY_CHANNEL_ID
-		// message.content === '#verify'
-	) {
-		const worker = await createWorker('eng');
+		message.author.bot ||
+		message.content !== '#verify' ||
+		message.attachments.size === 0 ||
+		message.channel.id !== config.VERIFY_CHANNEL_ID
+	)
+		return;
 
-		await Promise.all(
-			message.attachments.map(async (attachment) => {
-				const msg = await message.reply({
-					content: 'Processing image...',
-				});
+	const worker = await createWorker('eng');
 
-				const fileArrayBuffer = await axios.get<ArrayBuffer>(
-					attachment.url,
-					{
-						responseType: 'arraybuffer',
-					}
-				);
+	await Promise.all(
+		message.attachments.map(async (attachment) => {
+			const msg = await message.reply('Processing image...');
 
-				const fileBuffer = Buffer.from(fileArrayBuffer.data);
+			const { data: fileArrayBuffer } = await axios.get<ArrayBuffer>(
+				attachment.url,
+				{ responseType: 'arraybuffer' }
+			);
 
-				const dimension = sizeOf(fileBuffer);
+			const fileBuffer = Buffer.from(fileArrayBuffer);
 
-				const pixelTopLeftM = {
-					x: Math.round((dimension.width as number) * topLeftM.x),
-					y: Math.round((dimension.height as number) * topLeftM.y),
-				};
+			const dimension = sizeOf(fileBuffer);
 
-				const pixelWM = Math.round(
-					(buttomRightM.x - topLeftM.x) * (dimension.width as number)
-				);
-				const pixelHM = Math.round(
-					(buttomRightM.y - topLeftM.y) * (dimension.height as number)
-				);
+			const pixelTopLeftM = {
+				x: Math.round((dimension.width as number) * topLeftM.x),
+				y: Math.round((dimension.height as number) * topLeftM.y),
+			};
 
-				const pixelTopLeftP = {
-					x: Math.round((dimension.width as number) * topLeftP.x),
-					y: Math.round((dimension.height as number) * topLeftP.y),
-				};
+			const pixelWM = Math.round(
+				(buttomRightM.x - topLeftM.x) * (dimension.width as number)
+			);
+			const pixelHM = Math.round(
+				(buttomRightM.y - topLeftM.y) * (dimension.height as number)
+			);
 
-				const pixelWP = Math.round(
-					(buttomRightP.x - topLeftP.x) * (dimension.width as number)
-				);
-				const pixelHP = Math.round(
-					(buttomRightP.y - topLeftP.y) * (dimension.height as number)
-				);
+			const MOcr = await worker.recognize(fileBuffer, {
+				rectangle: {
+					top: pixelTopLeftM.y,
+					left: pixelTopLeftM.x,
+					width: pixelWM,
+					height: pixelHM,
+				},
+			});
 
-				// const image = await sharp(fileBuffer.data)
-				// 	.extract({ left: 1108, top: 463, width: 378, height: 75 })
-				// 	.toBuffer();
+			const POcr = await worker.recognize(fileBuffer);
 
-				let text = '';
+			const text = MOcr.data.text || POcr.data.text;
 
-				const MOcr = await worker.recognize(fileBuffer, {
-					rectangle: {
-						top: pixelTopLeftM.y,
-						left: pixelTopLeftM.x,
-						width: pixelWM,
-						height: pixelHM,
-					},
-				});
+			const user = message.guild.members.cache.get(message.author.id);
+			const userRoles = user?.roles.cache.map((role) => role.id);
 
-				const POcr = await worker.recognize(fileBuffer);
+			for (const alliance of config.ALLIANCE_LIST) {
+				if (userRoles?.includes(alliance.role_id)) {
+					await msg.edit({
+						content: '',
+						embeds: [
+							new EmbedBuilder()
+								.setDescription(
+									"You have already been verified, please don't try to verify again!"
+								)
+								.setTitle('Alliance Verify')
+								.setThumbnail(
+									message.client.user.displayAvatarURL()
+								)
+								.setFooter({
+									iconURL: message.author.displayAvatarURL(),
+									text: `Requested by ${message.author.displayName}`,
+								})
+								.setColor('Red'),
+						],
+					});
+					break;
+				} else if (text.trim().includes(alliance.name)) {
+					await user?.roles.add(alliance.role_id);
 
-				text = MOcr.data.text == '' ? POcr.data.text : MOcr.data.text;
-
-				const user = message.guild.members.cache.get(message.author.id);
-				const userRoles = user?.roles.cache.map((role) => role.id);
-
-				// await message.channel.send({
-				// 	content: text,
-				// });
-
-				for (const alliance of config.ALLIANCE_LIST) {
-					if (userRoles?.includes(alliance.role_id)) {
-						await msg.edit({
-							content: '',
-							embeds: [
-								new EmbedBuilder()
-									.setDescription(
-										"You have aleardy been verified, please don't try to verify again!"
-									)
-									.setTitle('Alliance Verify')
-									.setThumbnail(
-										message.client.user.displayAvatarURL()
-									)
-									.setFooter({
-										iconURL:
-											message.author.displayAvatarURL(),
-										text: `Requested by ${message.author.displayName}`,
-									})
-									.setColor('Red'),
-							],
-						});
-						break;
-					} else if (text.trim().includes(alliance.name)) {
-						const user = message.guild.members.cache.get(
-							message.author.id
-						);
-
-						await user?.roles.add(alliance.role_id);
-						// send croped image to user with channel send
-
-						await msg.edit({
-							content: '',
-							embeds: [
-								new EmbedBuilder()
-									.setDescription('You have been verified!')
-									.setTitle('Alliance Verify')
-									.setThumbnail(
-										message.client.user.displayAvatarURL()
-									)
-									.setFooter({
-										iconURL:
-											message.author.displayAvatarURL(),
-										text: `Requested by ${message.author.displayName}`,
-									}),
-							],
-						});
-						break;
-					}
+					await msg.edit({
+						content: '',
+						embeds: [
+							new EmbedBuilder()
+								.setDescription('You have been verified!')
+								.setTitle('Alliance Verify')
+								.setThumbnail(
+									message.client.user.displayAvatarURL()
+								)
+								.setFooter({
+									iconURL: message.author.displayAvatarURL(),
+									text: `Requested by ${message.author.displayName}`,
+								})
+								.setColor('Green'),
+						],
+					});
+					break;
 				}
-			})
-		);
+			}
 
-		await worker.terminate();
-	}
+			if (msg.content === 'Processing image...') {
+				await msg.edit({
+					content: '',
+					embeds: [
+						new EmbedBuilder()
+							.setDescription(
+								'Your alliance name is not found! Please try again!'
+							)
+							.setTitle('Alliance Verify')
+							.setThumbnail(
+								message.client.user.displayAvatarURL()
+							)
+							.setFooter({
+								iconURL: message.author.displayAvatarURL(),
+								text: `Requested by ${message.author.displayName}`,
+							})
+							.setColor('Red'),
+					],
+				});
+			}
+		})
+	);
+
+	await worker.terminate();
 }
